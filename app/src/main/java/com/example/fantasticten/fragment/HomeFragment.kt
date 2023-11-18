e50329128949d35065ee0f90ab6300b8af7b677c
Azizzarkasyi <koyekkasyi@gmail.com>
2023-11-18 23:56:17 +0700

    tanggal 18  november
    Signed-off-by: Azizzarkasyi <koyekkasyi@gmail.com>

--- app/src/main/java/com/example/fantasticten/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/fantasticten/fragment/HomeFragment.kt
@@ -1,11 +1,17 @@
 package com.example.fantasticten.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 import com.example.fantasticten.R
+import com.example.fantasticten.home_feature.Daftar_Antrian
 
 class HomeFragment : Fragment() {
 
@@ -13,7 +19,32 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val next= inflater.inflate(R.layout.fragment_home, container, false)
+        val imageSlider = next.findViewById<ImageSlider>(R.id.image_slider)
+        val konsultasi = next.findViewById<ImageButton>(R.id.konsultasi)
+        val program = next.findViewById<ImageButton>(R.id.program)
+        val tretment = next.findViewById<ImageButton>(R.id.treatment)
+        val daftarAntrian = next.findViewById<ImageButton>(R.id.dftrantrian)
+        val dokter = next.findViewById<ImageButton>(R.id.doktor)
+        val lokasi = next.findViewById<ImageButton>(R.id.lokasi)
+        val imageList = ArrayList<SlideModel>()
+
+        imageList.add(SlideModel(R.drawable.slide1))
+        imageList.add(SlideModel("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUDsuqzb99RjFg0oi-3fWH5D0Dz2Q82Y2GDg&usqp=CAU"))
+        imageList.add(SlideModel("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkQ5UYCFYLZUL71cWks1JTt7pujkfQYHXDnA&usqp=CAU"))
+        imageList.add(SlideModel("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtCuvnsFV_MmWxB65QScMbQDW4kqDD26j0pA&usqp=CAU"))
+        imageSlider.setImageList(imageList,ScaleTypes.FIT)
+
+        daftarAntrian.setOnClickListener{
+            val intent = Intent(activity,Daftar_Antrian::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+        return next
     }
 }
