b4b66cb147b0549baec525f24c04f5cb8ffa7dc8
Azizzarkasyi <koyekkasyi@gmail.com>
2023-12-12 09:50:27 +0700

    merge ya

---
+++ app/src/main/java/com/example/fantasticten/home_feature/chat/mobileChatLeft.kt
@@ -0,0 +1,10 @@
+package com.example.fantasticten.home_feature.chat
+
+data class mobileChatLeft(
+    val chatId1 :String?,
+    val chatMobile1 : String
+){
+    constructor(): this("", ""){
+
+    }
+}
